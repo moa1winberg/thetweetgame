ee1e631fd7de0f80ed47dc91a4b9fbc3ffe1bc3e
moa1winberg <32701745+moa1winberg@users.noreply.github.com>
2018-01-20 01:39:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
 // site preloader -- also uncomment the div in the header and the css style for #preloader
 $(window).load(function(){
-    $('#preloader').fadeOut('slow',function(){$(this).remove();});
+    $('.se-pre-con').fadeOut('slow',function(){$(this).remove();});
 });
 
 });
